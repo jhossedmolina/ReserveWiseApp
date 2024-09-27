0d06c564d855ec4b04f83cc96aac66addcf8e80b
Jhossed Molina <jhossedmolina@gmail.com>
2024-09-27 01:41:19 -0500

    Adjustments for project compilation

--- ReserveWiseAppBackend/src/ReserveWiseAppBackend.Infraestructure/obj/Debug/net8.0/ReserveWiseAppBackend.Infraestructure.AssemblyInfo.cs
+++ ReserveWiseAppBackend/src/ReserveWiseAppBackend.Infraestructure/obj/Debug/net8.0/ReserveWiseAppBackend.Infraestructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReserveWiseAppBackend.Infraestructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d937bc407206ad5cca1d637e3564a5070a217b44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e57f904741bea66ee23559bc312ef78a3350f4e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReserveWiseAppBackend.Infraestructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReserveWiseAppBackend.Infraestructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
